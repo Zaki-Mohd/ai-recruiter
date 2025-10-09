70c14eee19acf710d7240f4430f0d9e200b7f1c4
Zaki-Mohd <zakilol21427@gmail.com>
2025-10-09 18:45:39 +0530

    AI-FEEDBACK

--- app/api/ai-feedback/route.jsx
+++ app/api/ai-feedback/route.jsx
@@ -2,36 +2,29 @@ import { FEEDBACK_PROMPT } from "@/services/Constants";
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
-export async function POST(req){
-        const {Conversation} = await req.json();
-        const FINAL_PROMPT = FEEDBACK_PROMPT.replace('{{conversation}}',JSON.stringify(Conversation));
+const openai = new OpenAI({
+  baseURL: "https://openrouter.ai/api/v1",
+  apiKey: process.env.OPEN_ROUTER_KEY,
+});
 
-         try {
-        
-          
-            const openai = new OpenAI({
-          baseURL: "https://openrouter.ai/api/v1",
-          apiKey: process.env.OPEN_ROUTER_KEY,
-        })
-        
-         const completion = await openai.chat.completions.create({
-            model: "google/gemini-pro-1.5",
-            messages: [
-              { role: "user", content: FINAL_PROMPT }
-            ],
-            max_tokens: 1000,
-         
-          })
-          console.log(completion.choices[0].message)
-          return NextResponse.json(completion.choices[0].message)
-        
-          }
-          catch(e){
-            console.log(e);
-            return NextResponse.json(e)
-          }
+export async function POST(req) {
+  const { Conversation } = await req.json();
+  const FINAL_PROMPT = FEEDBACK_PROMPT.replace(
+    "{{conversation}}",
+    JSON.stringify(Conversation)
+  );
 
+  try {
+    const chatCompletion = await openai.chat.completions.create({
+      model: "openai/gpt-3.5-turbo",
+      messages: [{ role: "user", content: FINAL_PROMPT }],
+      max_tokens: 1000,
+    });
+    const feedback = chatCompletion.choices[0].message.content;
 
-
-
+    return NextResponse.json({ content: feedback });
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({ error: e });
+  }
 }
