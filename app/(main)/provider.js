27f26cc3b58fcc921be0dc1194c929ee71000d72
TrlRizu <trl.rizu2020@gmail.com>
2025-10-14 16:59:13 +0800

    Added a button to toggle sidebar on mobile

--- app/(main)/provider.js
+++ app/(main)/provider.js
@@ -1,25 +1,32 @@
 "use client";
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
-import React from 'react'
-import AppSideBar, { AppSidebar } from './_components/AppSideBar'
-import WelcomeContainer from './dashboard/_components/WelcomeContainer'
 import Provider, { useUser } from '@/app/provider';
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import React from 'react';
+import AppSideBar, { AppSidebar } from './_components/AppSideBar';
+import WelcomeContainer from './dashboard/_components/WelcomeContainer';
 
 function DashboardProvider({children}) {
   return (
-    <Provider>
+    <Provider> 
 
-   
-    <SidebarProvider>
-        <AppSidebar />
+      <SidebarProvider>
+        <div className='flex min-h-screen w-full'>
+        {/* Issue #69 fix: sidebar: always open on website & drawer on mobile */}
+          <AppSidebar />
+                <div className="flex-1 flex flex-col p-4 md:p-10">
 
-              <div className='w-full p-10'>
-                 {/* <SidebarTrigger/> */}
-                 <WelcomeContainer /> 
-                {children}</div>
+                  {/* Mobile sidebar trigger (hamburger button) */}
+                  <div className="fixed w-10 h-10 bg-gray-500 rounded-full flex justify-center items-center md:hidden mb-4">
+                    <SidebarTrigger />
+                  </div>
 
-    </SidebarProvider>
-     </Provider>
+                  <WelcomeContainer />
+                  {children}
+                </div>
+
+        </div>
+      </SidebarProvider>
+    </Provider>
   
   )
 }
